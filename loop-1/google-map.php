f474ec8605503ec9de7af7a9ec1158d1b1fcc61f
calhouny <calhounhamilton@gmail.com>
2015-01-15 19:05:55 -0600

    deleted duplicates

--- loop-1/google-map.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-* Google Map
-*/
-?> 
-
-	<div class="map_canvas" id="map_canvas"><!--DISPLAY GOOGLE MAP HERE--></div>
--- loop-1/sidebar.php
+++
@@ -1,5 +0,0 @@
-				<!--SIDEBAR START-->
-                <div class="col-md-4 sidebar">
-                	<?php  if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('default-sidebar') ) : endif; ?>
-                </div><!--/.sidebar-->
-                <!--SIDEBAR END-->
